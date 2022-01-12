e59e4be719bab5f7e6978f8c5e36e8aba510b08b
H-Yuuki <yuukirinnma@gmail.com>
2022-01-13 00:30:12 +0900

    UITableView の作成

--- UITableView-tutorial/UITableView-tutorial/ViewController.swift
+++ UITableView-tutorial/UITableView-tutorial/ViewController.swift
@@ -7,7 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    let weeksName = ["月", "火", "水", "木", "金", "土", "日"]
+
+    // 描画するセルの数
+    // Fix を押すと自動で追加される①
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        return weeksName.count   // 7
+
+    }
+
+    // 描画するセルを生成する
+    // Fix を押すと自動で追加される②
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        // セルを作成, Identifier が "Cell" となっているセルを呼び出す
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = weeksName[indexPath.row]   // 表示する文字列
+        return cell
+
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
