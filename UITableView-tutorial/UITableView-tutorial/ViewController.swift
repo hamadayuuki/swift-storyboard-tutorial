69ac00feb27a7474e36d6cc3cbc8b39a6d48dbba
H-Yuuki <yuukirinnma@gmail.com>
2022-01-12 18:31:54 +0900

    UITableView のプロジェクトを作成

---
+++ UITableView-tutorial/UITableView-tutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITableView-tutorial
+//
+//  Created by 濵田　悠樹 on 2022/01/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
