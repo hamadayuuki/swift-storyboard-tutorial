a915d4ab9f52b41862d05f2c77096ec46efe5169
H-Yuuki <yuukirinnma@gmail.com>
2022-01-20 21:54:05 +0900

    Combine Justを使った基本的な実装

--- Combine.swift/Combine.swift/ViewController.swift
+++ Combine.swift/Combine.swift/ViewController.swift
@@ -15,6 +15,26 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        // 基本的な使い
+        // ① : パブリッシャーの作成
+        let publisher = Just(100)
+        
+        // ② : サブスクライバーの作成
+        let subscriber = Subscribers.Sink<Int, Never>(receiveCompletion: { completion in
+            switch completion {
+            case .finished:
+                print("実行完了")
+            case .failure:
+                print("エラー")
+            }
+            
+        }, receiveValue: { value in
+            print("取得結果")
+            print(value)
+        })
+        
+        // ③ : パブリッシャーとサブスクライバーをサブスクライブする
+        publisher.subscribe(subscriber)
         
     }
     
