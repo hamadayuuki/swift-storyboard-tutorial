35cc308cf83caf0d868641a412a2d0d763318cb0
H-Yuuki <yuukirinnma@gmail.com>
2022-02-06 22:52:47 +0900

    Timer をPublisher化し、現在時刻を取得し続ける

--- Combine/Combine_2/Combine_2/ViewController.swift
+++ Combine/Combine_2/Combine_2/ViewController.swift
@@ -7,10 +7,14 @@
 
 /*
  Combineの学習用
+ 
+ ! Combineの実行結果が出ない時は、cancellable を クラス外に グローバル変数として定義して、Subscribe時に cancellable を使用する
+   → 関数内で定義すると、関数を抜けた後、受信できなくなるから
  */
 
 import UIKit
 import Combine
+import Foundation   // Timer
 
 // イベントの送受信を仲介
 // イベントは「文字列を渡して表示させる」というもの
@@ -19,6 +23,13 @@ public let subject = PassthroughSubject<String, Never>()   // <受信する型, 
 // イベントを送信する
 // リストを Publisher に変更できる,
 public let listPublisher = ["1", "2", "3", "4", "5"].publisher   // 型は Publishers.Sequence<[String], Never>
+
+// タイマーを Publisher に変更
+// Subscribeで .aoutconnect() を行う, Subscribe(sinkやassign)を行うため
+public var timerPublisher = Timer.publish(every: 1.0, on: .main, in: .common)
+
+// 受信結果を保持し続けるために使用
+public var cancellable: Cancellable?
 
 class ViewController: UIViewController {
 
@@ -35,6 +46,8 @@ class ViewController: UIViewController {
             })
          */
         
+        
+        /*
         // イベントの送信
         let receiver = Receiver()
         subject.send("あ")
@@ -44,6 +57,16 @@ class ViewController: UIViewController {
         subject.send("え")
         subject.send("お")
         subject.send(completion: .finished)   // イベント終了を送信
+         */
+        
+        let receiver = Receiver()
+        /*
+        cancellable = Timer.publish(every: 1, on: .main, in: .default)
+               .autoconnect()
+               .sink(receiveValue: { value in
+                   print(value)
+               })
+         */
         
     }
 }
@@ -89,7 +112,7 @@ final class Receiver {
          → sink や assign を使用できなくなる
            → 実行を役割毎に分けて、storeで一つの subscription にまとめる
          */
-        
+        /*
         listPublisher
             //.assign(to: \.value, on: SomeObject())
             .sink(receiveCompletion: { completion in
@@ -102,6 +125,22 @@ final class Receiver {
         listPublisher
             .assign(to: \.value, on: SomeObject())
             .store(in: &subscriptions)   // 1つの subscription にまとめる
+         */
+        
+        // クラス外に定義した cancellable を使用しないと、実行結果を確認できない
+        /*
+         listPublisher        :  Sequence<Array<String>, Never>(sequence: ["1", "2", "3", "4", "5"])
+         listPublisher type   :  Sequence<Array<String>, Never>
+         timerPublisher       :  (extension in Foundation):__C.NSTimer.TimerPublisher
+         timerPublisher type  :  TimerPublisher
+         → TimerPublisher は,他のPublisher とは 型が異なる.
+         */
+        cancellable = timerPublisher
+            .autoconnect()
+            .sink { value in
+                print(value)
+            }
+        
     }
 }
 
