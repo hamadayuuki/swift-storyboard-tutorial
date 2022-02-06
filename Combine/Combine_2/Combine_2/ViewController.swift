e53383c8ac673a42220c8c0140e44385a943a96e
H-Yuuki <yuukirinnma@gmail.com>
2022-02-06 19:23:57 +0900

    複数のSubscriptionを作成し、送信を行う

--- Combine/Combine_2/Combine_2/ViewController.swift
+++ Combine/Combine_2/Combine_2/ViewController.swift
@@ -21,6 +21,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        /*
         // イベントの受け取り
         let subscription = subject
             .sink(receiveCompletion: { completion in
@@ -28,10 +29,13 @@ class ViewController: UIViewController {
             }, receiveValue:{ value in
                 print("Receive Value : ", value)
             })
+         */
         
         // イベントの送信
+        let receiver = Receiver()
         subject.send("あ")
         subject.send("い")
+        receiver.subscription1.cancel()   // subscription1 を中止する
         subject.send("う")
         subject.send("え")
         subject.send("お")
@@ -42,3 +46,24 @@ class ViewController: UIViewController {
 
 }
 
+// 受け取り先を2つ作成
+// final : 継承を禁止する
+final class Receiver {
+    // 受け取りを2つ用意
+    let subscription1: AnyCancellable
+    let subscription2: AnyCancellable
+    
+    init() {
+        // 1つ目の受け取り
+        subscription1 = subject
+            .sink{ value in
+                print("[1] subscription : ", value)
+            }
+        
+        // 2つ目の受け取り
+        subscription2 = subject
+            .sink{ value in
+                print("[2] subscription : ", value)
+            }
+    }
+}
